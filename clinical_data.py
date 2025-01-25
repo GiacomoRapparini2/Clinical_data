304ed1a206f5dbe34d6e006d76b26a7c381441a2
“Giacomo <giacomo.rapparini@studio.unibo.it>
2025-01-25 10:29:39 +0100

    Updated the results folder.

--- clinical_data.py
+++ clinical_data.py
@@ -3,6 +3,7 @@ import json
 import os
 import matplotlib.pyplot as plt
 import itertools
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 # Load the path to the csv file from paths.json
@@ -23,6 +24,9 @@ res_dir = paths['results_folder']['path']
 
 # Load the ROI volumes
 roi_volumes = pd.read_csv(os.path.join(res_dir, 'roi_volumes.csv'))
+
+# Preprocess the 'patient' column to extract only the number
+roi_volumes['patient'] = roi_volumes['patient'].astype(str).str.extract(r'(\d+)').astype(int)
 
 # Load the medians
 medians = pd.read_csv(os.path.join(res_dir, 'median_results.csv'))
@@ -46,7 +50,6 @@ clinical_features = clinical_data.columns
 print(clinical_features)
 patients = medians['patient'].unique()
 
-
 # Create a DataFrame with the first column as 'patient' and other 4 as the 
 # absolute median difference between the brain and the roi for the 4 features
 
@@ -64,11 +67,19 @@ for patient in patients:
 
 print(median_diff.head())
 
-# Merge the clinical data with the median_diff DataFrame
+# Merge the clinical data with the median_diff DataFrame and the roi_volumes DataFrame
+clinical_data = clinical_data.merge(roi_volumes, on='patient')
 clinical_data = clinical_data.merge(median_diff, on='patient')
 
-# Drop the columns 'patient', 'date', 'tici_end' and 'sex' for correlation calculation
-correlation_data = clinical_data.drop(columns=['patient', 'date', 'tici_end', 'sex'])
+# Create a folder to save the results
+clin_res = os.path.join(res_dir, 'clinical_results')
+if not os.path.exists(clin_res):
+    os.makedirs(clin_res)
+
+# Correlation analysis
+
+# Drop the columns 'patient', 'date', 'tpa', 'tici_end' and 'sex' for correlation calculation
+correlation_data = clinical_data.drop(columns=['patient', 'date', 'tpa', 'tici_end', 'sex'])
 
 # Calculate the correlation matrix
 correlation_matrix = correlation_data.corr()
@@ -76,25 +87,26 @@ correlation_matrix = correlation_data.corr()
 # Print the correlation matrix
 print(correlation_matrix)
 
-# Optionally, plot the correlation matrix
+# Plot the correlation matrix
 plt.figure(figsize=(10, 8))
 plt.matshow(correlation_matrix)
 plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=90)
 plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 plt.colorbar()
 plt.title('Correlation Matrix', pad=20)
-plt.savefig(os.path.join(res_dir, 'correlation_matrix.png'))
+plt.savefig(os.path.join(clin_res, 'correlation_matrix.png'))
 
 # Create a DataFrame with the correlation values
-correlation_values = pd.DataFrame(columns=['feature1', 'feature2', 'correlation'])
+correlation_values_list = []
 for col1, col2 in itertools.combinations(correlation_matrix.columns, 2):
-    correlation_values = correlation_values.append({'feature1': col1, 'feature2': col2, 'correlation': correlation_matrix.loc[col1, col2]}, ignore_index=True)
+    correlation_values_list.append({'feature1': col1, 'feature2': col2, 'correlation': correlation_matrix.loc[col1, col2]})
+correlation_values = pd.DataFrame(correlation_values_list)
 
 # Sort the DataFrame by the correlation values
 correlation_values = correlation_values.sort_values(by='correlation', ascending=False)
 
 # Save the correlation values to a csv file 
-correlation_values.to_csv(os.path.join(res_dir, 'correlation_clinical.csv'), index=False)
+correlation_values.to_csv(os.path.join(clin_res, 'correlation_clinical.csv'), index=False)
 
 # Create scatter plots for columns with correlation > 0.5 and < 1
 high_corr_pairs = [(col1, col2) for col1, col2 in itertools.combinations(correlation_matrix.columns, 2) 
@@ -106,20 +118,37 @@ for col1, col2 in high_corr_pairs:
     plt.xlabel(col1)
     plt.ylabel(col2)
     plt.title(f'Scatter plot between {col1} and {col2}')
-    plt.show()
-    #plt.savefig(os.path.join(res_dir, f'scatter_{col1}_{col2}.png'))
+    #plt.show()
+    plt.savefig(os.path.join(clin_res, f'scatter_{col1}_{col2}.png'))
     plt.close()
 
-# Drop the columns 'mrs_basal', 'mrs_90d', 'tici_end_code'  for PCA calculation
-pca_data = correlation_data.drop(columns=['mrs_basal', 'mrs_90d', 'tici_end_code'])
+# PCA analysis
+
+# Drop the columns 'aspects', 'mrs_basal', 'mrs_90d', 'tici_end_code'  for PCA calculation
+pca_data = correlation_data.drop(columns=['aspects', 'mrs_basal', 'mrs_90d', 'tici_end_code'])
+
+# Drop columns with NaN values
+pca_data = pca_data.dropna(axis=1)
+
+# Initialize the scaler
+scaler = StandardScaler()
+
+# Standardize the data
+data_scaled = scaler.fit_transform(pca_data)
+
+# Convert back to a DataFrame
+data_scaled_df = pd.DataFrame(data_scaled, columns=pca_data.columns)
+
+# Check the standardized data
+print(data_scaled_df.head())
 
 # Apply PCA to the data frame
 pca = PCA(n_components=3)
-pca_result = pca.fit_transform(pca_data)
+pca_result = pca.fit_transform(data_scaled_df)
 
 # Create a DataFrame with the PCA results
-pca_df = pd.DataFrame(data=pca_result, columns=['PC1', 'PC2'])
+pca_df = pd.DataFrame(data=pca_result, columns=['PC1', 'PC2', 'PC3'])
 pca_df['patient'] = clinical_data['patient']
 
 # Save the PCA results to a csv file
-pca_df.to_csv(os.path.join(res_dir, 'pca_results.csv'), index=False)
+pca_df.to_csv(os.path.join(clin_res, 'pca_clinical.csv'), index=False)
